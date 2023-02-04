169e5864f6e934a12a05ff9f26032076811999f2
Lunime ૮ ˶ᵔ ᵕ ᵔ˶ ა <120704613+ruihq@users.noreply.github.com>
2023-02-04 15:33:37 +0800

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,10 @@
+# File : installer.sh
+# Owner : ruihq <lunibusiness6@gmail.com>
+sudo apt update
+sudo apt upgrade
+sudo apt install snapd 
+sudo apt install openssh-server 
+sudo snap install ngrok
+sleep 2
+clear
+echo finished
